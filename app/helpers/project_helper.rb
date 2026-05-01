6e1a27dfb84016fe37d2b324d5264538ad5adf8d
sankalpdevtester <ohofufic0@gmail.com>
2026-05-01 06:11:03 +0530

    feat: add utility function to calculate project popularity score

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,13 +1,19 @@
-# app/helpers/project_helper.rb
-
 module ProjectHelper
-  # Existing code...
+  def self.calculate_popularity_score(project)
+    # Calculate popularity score based on project's watch count, star count, and fork count
+    watch_count = project.watchers.count
+    star_count = project.stars.count
+    fork_count = project.forks.count
 
-  # Calculate project popularity score based on number of reviews and watchers
-  def calculate_popularity_score(project)
-    reviews = project.reviews.count
-    watchers = project.watchers.count
-    score = (reviews * 0.6) + (watchers * 0.4)
-    score.round(2)
+    # Assign weights to each factor
+    watch_weight = 0.3
+    star_weight = 0.4
+    fork_weight = 0.3
+
+    # Calculate popularity score
+    popularity_score = (watch_count * watch_weight) + (star_count * star_weight) + (fork_count * fork_weight)
+
+    # Return the popularity score
+    popularity_score
   end
 end
